f89ea891b519a12a36e19d309d0d5e498b5ac0e3
makrenka <test@example.com>
2022-09-20 18:27:17 +0300

    add button

---
+++ src/components/button/button.js
@@ -0,0 +1,5 @@
+import './button.scss';
+
+export const Button = () => {    
+    return '<button class="button">Click</button>';    
+}
--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import "./main.scss";
+import { Button } from './components/button/button';
 
 const add123 = (a, b) => a + b;
+let root  = document.querySelector('#root');
+
+root.insertAdjacentHTML('beforeend', Button());
 
 const res = add123(222, 3);
 console.log("Helloasda");
