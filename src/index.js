06334d503fdc98893b27328969b93f1b3ee214d6
makrenka <test@example.com>
2022-09-23 21:31:40 +0300

    add functions

--- src/index.js
+++ src/index.js
@@ -1,10 +1,31 @@
 import "./main.scss";
 import { Button } from './components/button/button';
 
-const add123 = (a, b) => a + b;
+
 let root  = document.querySelector('#root');
 
 root.insertAdjacentHTML('beforeend', Button());
 
-const res = add123(222, 3);
-console.log("Helloasda");
+
+
+// const add = (...num) => {
+//     let sum = 0;
+//     for (let arg of num) {
+//         sum += arg;
+//     };
+//     return sum;
+// };
+
+// add(1, 2, 3);
+// console.log(add);
+
+// const min = (a, b) => {
+//     if (a > b) {
+//         return b;
+//     } else {
+//         return a;
+//     }
+// };
+
+// min(1, 2);
+
