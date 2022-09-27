a980c106067d1ddbcc0f540957bef24000191a56
makrenka <test@example.com>
2022-09-27 21:30:23 +0300

    add lesson

--- src/index.js
+++ src/index.js
@@ -7,25 +7,56 @@ let root  = document.querySelector('#root');
 root.insertAdjacentHTML('beforeend', Button());
 
 
+// Рекурсия
+// Сложить числа 1 + 2 + 3 + ...
+// const sumTo = (n) => {
+//     if (n === 1) return n;
+//     return n + sumTo(n - 1);
+// }
 
-// const add = (...num) => {
-//     let sum = 0;
-//     for (let arg of num) {
-//         sum += arg;
-//     };
-//     return sum;
-// };
+// sumTo (10);
 
-// add(1, 2, 3);
-// console.log(add);
-
-// const min = (a, b) => {
-//     if (a > b) {
-//         return b;
-//     } else {
-//         return a;
+// Рекурсия - пример с объектами
+// const list = {
+//     a: "a",
+//     b: "b",
+//     next: {
+//         a: "a-1",
+//         b: "b-1",
+//         next: {
+//             a: "a-2",
+//             b: "b-2",
+//             next: null
+//         }
 //     }
 // };
 
-// min(1, 2);
+// function showA(list) {
+//     console.log(list.a);
+//     if (list.next) return showA(list.next);
+//     return;
+// };
+// showA(list);
 
+// while (list) {
+//     console.log(list.a);
+//     list = list.next;
+// };
+
+
+
+// Замыкание 
+// Сложение чисел
+// const sum = (a) => {
+//     return (b) => {
+//         return a + b;
+//     }
+// };
+
+// console.log(sum(10)(20));
+
+// Вырезать из строки
+// const string = "aaa bbb ccc";
+
+// const searchValue = string.indexOf('bbb')
+// console.log(string.slice(searchValue, searchValue + 3))
