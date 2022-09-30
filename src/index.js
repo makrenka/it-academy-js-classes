b9ba906695517e6e2bc565f884776736d643831c
makrenka <test@example.com>
2022-09-30 21:43:13 +0300

    add reverse and forEach

--- src/index.js
+++ src/index.js
@@ -8,16 +8,24 @@ import "./main.scss";
 
 
 // let array = [2, 3, 4, undefined, null, "", false, 5, 6, 0];
-
 // let compact = (arr) => {
 //     return arr.filter(Boolean);
 // }
-
 // console.log(compact(array));
 
 // let arr = [1, 2, 3, 4, 5];
-
 // let suminput = arr.reduce((sum, current) => sum + current, 0);
-
 // console.log(suminput)
 
+
+// let arr = [1, 2, 3, 4, 5];
+// const reverse = () => arr.reverse();
+// console.log(reverse());
+
+
+// Написать, что в forEach по капотом
+const forEach = (item, i, arr) => {
+    for (const item in arr) {
+        callback();
+    }
+}
