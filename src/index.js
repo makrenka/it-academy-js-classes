46ba6a33bb830aa07288a36d7fa47c1769a6a706
makrenka <smokrenko79@gmail.com>
2022-10-11 21:26:02 +0300

    add search in dom

--- src/index.js
+++ src/index.js
@@ -1,31 +1,11 @@
 import "./main.scss";
-import { Button } from './components/button/button';
 
 
-let root  = document.querySelector('#root');
+const tableId = document.getElementById('age-table');
+const labelTag = tableId.getElementsByTagName('label');
+const tdName = document.getElementsByName('age')[0];
+const formName = document.getElementsByName('search');
+// const inputForm = formName.getElementsByTagName('input');
 
-root.insertAdjacentHTML('beforeend', Button());
 
-
-
-// const add = (...num) => {
-//     let sum = 0;
-//     for (let arg of num) {
-//         sum += arg;
-//     };
-//     return sum;
-// };
-
-// add(1, 2, 3);
-// console.log(add);
-
-// const min = (a, b) => {
-//     if (a > b) {
-//         return b;
-//     } else {
-//         return a;
-//     }
-// };
-
-// min(1, 2);
-
+console.log(formName);
